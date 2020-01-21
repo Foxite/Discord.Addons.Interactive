fa8fb3b5a086f6a5b4caa40470d9a5d2ed74d1f0
foxite <d.kok.2000@gmail.com>
2020-01-21 15:43:42 +0100

    blackjack initiation

---
+++ SocketSampleBot/BlackjackModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SampleBot {
+	public class BlackjackModule : InteractiveBase<SocketCommandContext> {
+		[Command("play blackjack", RunMode = RunMode.Async)]
+		public async Task PlayBlackjack(params IUser[] users) {
+			if (users == null) {
+				await ReplyAsync("Er moet minstens 1 andere speler zijn voor Blackjack.");
+				return;
+			}
+
+			await ReplyAsync("At least one player needs to accept the invitation to join. They have 60 seconds to do so.");
+
+			string[] positive = new[] { "yes", "join", "ok", "accept" };
+			string[] negative = new[] { "no", "ignore", "deny", "refuse" };
+
+			var joinedUsers = new HashSet<IUser>();
+			await foreach (SocketMessage msg in WaitAllPlayers(users, new Criteria<SocketMessage>(
+					new EnsureSourceUserCriterion(),
+					new EnsureSourceChannelCriterion(),
+					new EnsureOneOfCriterion(new[] { positive, negative }.SelectMany(s => s))
+				))) {
+				if (positive.Contains(msg.Content.ToLower())) {
+					joinedUsers.Add(msg.Author);
+					await ReplyAsync("Confirmed: " + ((msg.Author as IGuildUser)?.Nickname ?? msg.Author.Username) + " is in the game.");
+				} else if (negative.Contains(msg.Content.ToLower())) {
+					await ReplyAsync("Confirmed: " + ((msg.Author as IGuildUser)?.Nickname ?? msg.Author.Username) + " is not playing.");
+				}
+			}
+			if (joinedUsers.Count == 0) {
+				await ReplyAsync("Can't play the game because nobody has accepted the invitation.");
+			} else {
+				await PlayBlackjack(joinedUsers);
+			}
+		}
+
+		private async IAsyncEnumerable<SocketMessage> WaitAllPlayers(IEnumerable<IUser> users, ICriterion<SocketMessage> criterion) {
+			var usersRemaining = new HashSet<IUser>(users);
+			SocketMessage result;
+			while (usersRemaining.Count > 0 && (result = await NextMessageAsync(criterion, TimeSpan.FromSeconds(60))) != null) {
+				yield return result;
+			}
+		}
+
+		private async Task PlayBlackjack(IEnumerable<IUser> users) {
+
+		}
+	}
+}
---
+++ SocketSampleBot/EnsureOneOfCriterion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SampleBot {
+	public class EnsureOneOfCriterion : ICriterion<SocketMessage> {
+		private IEnumerable<string> m_Values;
+
+		public EnsureOneOfCriterion(IEnumerable<string> values) {
+			m_Values = values.Select(val => val.ToLower());
+		}
+
+		public EnsureOneOfCriterion(params string[] values) {
+			m_Values = values.Select(val => val.ToLower());
+		}
+
+		public Task<bool> JudgeAsync(SocketCommandContext sourceContext, SocketMessage parameter) => Task.FromResult(m_Values.Contains(parameter.Content.ToLower()));
+	}
+}
